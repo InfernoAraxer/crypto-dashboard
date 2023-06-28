42aaf5be7b7e9cb699fcebfa818639a9809b015e
InfernoAraxer <AT212355@gmail.com>
2023-06-28 17:00:15 -0400

    First commit and Created Inputs for Amount and Currencies

---
+++ src/components/CurrencyConverter.js
@@ -0,0 +1,49 @@
+import ExchangeRate from "./ExchangeRate";
+import { useState } from 'react';
+
+const CurrencyConverter = () => {
+    const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA'];
+    const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
+    const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC');
+    const [amount, setAmount] = useState(1);
+
+    return (
+      <div className="currency-converter">
+        <h2>Currency Converter</h2>
+        <div>
+            <table>
+                <tbody>
+                <tr>
+                    <td>Primary Currency: </td>
+                    <td>
+                        <input type='number' name='currency-amount-1' value={amount} onChange={(e) => setAmount(e.target.value)}/>
+                    </td>
+                    <td>
+                        <select value={chosenPrimaryCurrency} name="currency-option-1" className="currency-options" onChange={(e) => setChosenPrimaryCurrency(e.target.value)}>
+                            {currencies.map( (currency, _index) => (<option key={_index}>{currency}</option>))}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Secondary Currency: </td>
+                    <td>
+                        <input type='number' name='currency-amount-2' value={""}/>
+                    </td>
+                    <td>
+                    <select value={chosenSecondaryCurrency} name="currency-option-2" className="currency-options" onChange={(e) => setChosenSecondaryCurrency(e.target.value)}>
+                            {currencies.map( (currency, _index) => (<option key={_index}>{currency}</option>))}
+                        </select>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+            <button id="convert-button">Convert</button>
+        </div>
+        <ExchangeRate/>
+      </div>
+    );
+  }
+  
+  export default CurrencyConverter;
+  
